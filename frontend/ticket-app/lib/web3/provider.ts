c1b8dfc1c2aba4bfb13892ef1940f4d7da9b21d7
hsjo12 <hsjo12@naver.com>
2025-06-22 22:54:20 +0900

    add the smart contract integration

---
+++ frontend/ticket-app/lib/web3/provider.ts
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+import {
+  ethers,
+  Contract,
+  InterfaceAbi,
+  Eip1193Provider,
+  BrowserProvider,
+} from "ethers";
+
+export const getProvider = () => {
+  const rpc = process.env.NEXT_PUBLIC_RPC;
+  return new ethers.JsonRpcProvider(rpc);
+};
+
+export const getReadOnlyContract = (target: string, abi: InterfaceAbi) => {
+  const rpc = process.env.NEXT_PUBLIC_RPC;
+  const provider = new ethers.JsonRpcProvider(rpc);
+  return new ethers.Contract(target, abi, provider);
+};
+
+export const getWriteContract = async (
+  target: string,
+  abi: InterfaceAbi,
+  walletProvider: Eip1193Provider
+) => {
+  const ethersProvider = new BrowserProvider(walletProvider);
+  const signer = await ethersProvider.getSigner();
+  return new Contract(target, abi, signer);
+};
